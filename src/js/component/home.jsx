3b95ed3f202c2882daf72fe88cd3022a83962c16
Bryan <168112800+BryanBV03@users.noreply.github.com>
2024-07-25 21:33:21 +0000

    TODOLIST

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
+import { IoMdClose } from "react-icons/io";
+
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+
+
+	//Hook de React
+	//const [state, setState] = useState ("Valor predeterminado")
+	const [nuevaTarea, setNuevaTarea] = useState("")
+	const [tareas, setTareas] = useState(["Limpiar la casa", "Lavar la ropa sucia", "Ir al supermercado"])
+
+	//Declaro mi Funcion
+	const handleClick = () => {
+		setTareas([...tareas, nuevaTarea]);
+	}
+
+
+	//Funcion que me permita borrar una tarea
+	const borrarTarea = (indice) => {
+		//Filtra todos los elementos menos el que tenga el indice que recibo.
+		const listaNueva = tareas.filter((tarea, i) => i !== indice);
+		setTareas(listaNueva);
+	}
+
+
+	// funcion de js que maneja eventos.
+	const handleChange = (evento) => {
+		//event.target.value
+		setNuevaTarea(evento.target.value);
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<div className="text-center" >
+			<div className="text-center mt-5">
+				<h1>
+					<strong>Lista de Tareas</strong>
+				</h1>
+			</div>
+			<div className="container contenedorDeTarea">
+				<div className="d-flex gap-2">
+					<input type="text" placeholder="Quieres añadir una Tarea?" className="form-control" onChange={handleChange} />
+					<button
+						onClick={handleClick} className="btn">
+						Agregar<br></br>Tarea
+					</button>
+				</div>
+				<h4> Tareas: {nuevaTarea}</h4>
+				<div className="contenedor">
+					<ul className="list-group">
+						{tareas.map((tarea, indice) => {
+							return (
+								<li className={
+									`list-group-item d-flex justify-content-between align-items-center ${indice % 2 === 0 ? "light-bg-subtle" : "bg-secondary"}`
+									}>
+									{tarea} 
+									< IoMdClose 
+									className="IoMdClose" onClick={() => borrarTarea(indice)} />
+								</li>
+							)
+						})}
+					</ul>
+				</div>
+			</div>
+		</div >
 	);
 };
 
